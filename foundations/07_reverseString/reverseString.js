9a15beec0dbb3586042405001b06f94e1616d9e3
Sultan <sultanrshah@tutamail.com>
2026-02-05 14:21:11 -0600

    07: Complete assignment with flying colors

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let final = "";
+    let arr = [];
+    for (let i = 0; i < input.length; i++) {
+        arr.push(input[i]);
+    }
 
+    for (let a = 0; a < input.length; a++) {
+        final += arr.pop();
+    }
+    return final;
 };
 
 // Do not edit below this line

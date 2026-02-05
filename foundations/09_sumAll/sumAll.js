2bc942a83d4cad65e82c978e75dd669ddfda5234
Sultan <sultanrshah@tutamail.com>
2026-02-05 14:50:55 -0600

    09: Complete assignment

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let final = 0;
+    const bigNum = Math.max(start, end);
+    const smallNum = Math.min(start, end);
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (start % 1 !== 0 || end % 1 !== 0) {
+        return "ERROR";
+    } else if (typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
+
+    for (let i = smallNum; i <= bigNum; i++) {
+        final += i;
+    }
+
+    return final
 };
 
 // Do not edit below this line

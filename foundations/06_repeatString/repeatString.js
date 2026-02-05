483ff0fff70ed1faad2fbe1e7030beb3d0550480
Sultan <sultanrshah@tutamail.com>
2026-02-05 14:11:27 -0600

    06: complete assignment

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    let string = "";
+    if (amount < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < amount; i++) {
+            string += word;
+        }
+    }
 };
 
 // Do not edit below this line
